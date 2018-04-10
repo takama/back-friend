cae180101e4932e9cdcd2e4fdfe75a07d6c39380
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-10 23:24:44 +0700

    Service setup

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,32 @@
 package service
 
-import "github.com/takama/back-friend/pkg/config"
+import (
+	"github.com/takama/back-friend/pkg/config"
+	"github.com/takama/back-friend/pkg/logger"
+	"github.com/takama/back-friend/pkg/logger/stdlog"
+	"github.com/takama/back-friend/pkg/system"
+	"github.com/takama/back-friend/pkg/version"
+)
 
 // Run the service
 func Run(cfg *config.Config) error {
+	// Setup logger
+	log := stdlog.New(&logger.Config{
+		Level: cfg.LogLevel,
+		Time:  true,
+		UTC:   true,
+	})
+
+	log.Info("Version:", version.RELEASE)
+	if cfg.LogLevel == logger.LevelDebug {
+		log.Warnf("%s log level is used", logger.LevelDebug.String())
+	}
+
+	// Wait signals
+	signals := system.NewSignals()
+	if err := signals.Wait(log, new(system.Handling)); err != nil {
+		log.Error(err)
+	}
+
 	return nil
 }
