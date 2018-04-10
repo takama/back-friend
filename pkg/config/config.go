7d2710e7b71e6ad368a4f88f75b0b3cc13027c21
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-10 23:22:28 +0700

    Configured default values

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,25 +1,37 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/takama/back-friend/pkg/helper"
+)
 
-// Default values: path to config file, host, port, etc
+// Default values: host, port, etc
 const (
 	// ServiceName contains a service name prefix which used in ENV variables
-	ServiceName = "BACK_FRIEND"
+	ServiceName = "BackFriend"
+
+	defaultHost = "0.0.0.0"
+	defaultPort = 8080
 )
 
 // Config - Service configuration
 type Config struct {
+	// Local service host
+	LocalHost string `split_words:"true"`
+	// Local service port
+	LocalPort int `split_words:"true"`
 }
 
 // New - returns new config record initialized with default values
 func New() *Config {
-	return &Config{}
+	return &Config{
+		LocalHost: defaultHost,
+		LocalPort: defaultPort,
+	}
 }
 
 // LoadFromEnv load configuration parameters from environment
 func (config *Config) LoadFromEnv() error {
 
-	// Load all rest environment
-	return envconfig.Process(ServiceName, config)
+	return envconfig.Process(helper.ToSnake(ServiceName), config)
 }
