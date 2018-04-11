942560ce5d1166033eb1385e1dbae325a8f7e9c5
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-11 19:24:50 +0700

    Register the handlers in service

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,11 +1,16 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/takama/back-friend/pkg/config"
+	"github.com/takama/back-friend/pkg/handlers"
 	"github.com/takama/back-friend/pkg/logger"
 	"github.com/takama/back-friend/pkg/logger/stdlog"
 	"github.com/takama/back-friend/pkg/system"
 	"github.com/takama/back-friend/pkg/version"
+
+	"github.com/takama/bit"
 )
 
 // Run the service
@@ -21,6 +26,22 @@ func Run(cfg *config.Config) error {
 	if cfg.LogLevel == logger.LevelDebug {
 		log.Warnf("%s log level is used", logger.LevelDebug.String())
 	}
+
+	// Define handlers
+	h := new(handlers.Handler)
+
+	// Register new router
+	r := bit.NewRouter()
+
+	// Response for undefined methods
+	r.SetupNotFoundHandler(h.Base(h.NotFound))
+
+	// Configure router
+	r.SetupMiddleware(h.Base)
+	r.GET("/", h.Root)
+
+	// Listen and serve handlers
+	go r.Listen(fmt.Sprintf("%s:%d", cfg.LocalHost, cfg.LocalPort))
 
 	// Wait signals
 	signals := system.NewSignals()
