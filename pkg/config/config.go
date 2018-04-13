e6d978eadc2c741b37d373a15f860a79995da031
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-13 21:07:17 +0700

    Database configuration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,9 +11,19 @@ const (
 	// ServiceName contains a service name prefix which used in ENV variables
 	ServiceName = "BackFriend"
 
-	defaultHost     = "0.0.0.0"
-	defaultPort     = 7117
-	defaultLogLevel = logger.LevelInfo
+	defaultHost       = "0.0.0.0"
+	defaultPort       = 7117
+	defaultLogLevel   = logger.LevelInfo
+	defaultDBLocation = "/var/lib/datastore"
+
+	// StubDriver defines driver name for testing
+	StubDriver = "stub"
+
+	// JSONDriver defines driver name for JSON files DB
+	JSONDriver = "json"
+
+	// PGDriver defines driver name for PostgreSQL DB
+	PGDriver = "postgresql"
 )
 
 // Config - Service configuration
@@ -24,20 +34,28 @@ type Config struct {
 	LocalPort int `split_words:"true"`
 	// Logging level in logger.Level notation
 	LogLevel logger.Level `split_words:"true"`
+	// Database type
+	DbType string `split_words:"true"`
+	// Database files location
+	DbLocation string `split_words:"true"`
 	// Database host
 	DbHost string `split_words:"true"`
 	// Database port
 	DbPort int `split_words:"true"`
 	// Database name
-	DbName int `split_words:"true"`
+	DbName string `split_words:"true"`
+	// Database username
+	DbUsername string `split_words:"true"`
 }
 
 // New - returns new config record initialized with default values
 func New() *Config {
 	return &Config{
-		LocalHost: defaultHost,
-		LocalPort: defaultPort,
-		LogLevel:  defaultLogLevel,
+		LocalHost:  defaultHost,
+		LocalPort:  defaultPort,
+		LogLevel:   defaultLogLevel,
+		DbType:     StubDriver,
+		DbLocation: defaultDBLocation,
 	}
 }
 
