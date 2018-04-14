a24d1f5332028e02436e579e2c05c7b3472d1107
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-14 17:50:56 +0700

    Added tables migration

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"net/url"
 
-	// PostgreSQL driver import
-	_ "github.com/lib/pq"
-
 	"github.com/takama/back-friend/pkg/config"
+
+	"github.com/lib/pq"
+)
+
+const (
+	// ErrorCodeDuplicateDatabase is code when create database but it already exists
+	ErrorCodeDuplicateDatabase = "42P04"
 )
 
 // PostgreSQL implements PostgreSQL driver
@@ -31,6 +35,17 @@ func NewPostgreSQL(cfg *config.Config) (pg *PostgreSQL, name string, err error) 
 	if err := pg.pool.QueryRow("SELECT version()").Scan(&name); err != nil {
 		return nil, name, err
 	}
+	if _, err := pg.pool.Exec(fmt.Sprintf("CREATE DATABASE %s", cfg.DbName)); err != nil {
+		if dbErr, ok := err.(*pq.Error); ok && dbErr.Code == ErrorCodeDuplicateDatabase {
+			// Database exists, there is no need to use migration
+			return pg, name, pg.pool.Ping()
+		}
+		return nil, name, err
+	}
+	// Database has been created, it should migrate up
+	if err = pg.MigrateUp(); err != nil {
+		return nil, name, err
+	}
 	return pg, name, pg.pool.Ping()
 }
 
@@ -44,10 +59,38 @@ func (pg PostgreSQL) Ready() bool {
 
 // MigrateUp migrates DB schema
 func (pg PostgreSQL) MigrateUp() error {
-	return nil
+	_, err := pg.pool.Exec(migrateUpData)
+	return err
 }
 
 // MigrateDown remove DB schema and data
 func (pg PostgreSQL) MigrateDown() error {
-	return nil
+	_, err := pg.pool.Exec(migrateDownData)
+	return err
 }
+
+const migrateUpData = `
+CREATE TABLE IF NOT EXISTS players (
+	id 				VARCHAR(128) PRIMARY KEY,
+	balance			NUMERIC(12,2) DEFAULT 0 CHECK (balance >= 0) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS tournaments (
+	id 				INT PRIMARY KEY,
+	is_finished 	BOOLEAN DEFAULT FALSE NOT NULL,
+	deposit			NUMERIC(12,2) DEFAULT 0 CHECK (deposit >= 0) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS bidders (
+	player_id		VARCHAR(128) REFERENCES players (id),
+	tournament_id	INT REFERENCES tournaments (id),
+	winner			BOOLEAN DEFAULT FALSE NOT NULL,
+	prize			NUMERIC(12,2) DEFAULT 0 CHECK (prize >= 0) NOT NULL,
+	backers			JSONB,
+	PRIMARY KEY (player_id, tournament_id)
+);
+`
+
+const migrateDownData = `
+DROP TABLE IF EXISTS bidders;
+DROP TABLE IF EXISTS tournaments;
+DROP TABLE IF EXISTS players;
+`
