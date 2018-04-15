360f9c60becd32f388a5fc28171383744fc6228a
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-16 03:07:21 +0700

    Postgres Player implementation

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -9,11 +9,15 @@ import (
 	"github.com/takama/back-friend/pkg/logger"
 
 	"github.com/lib/pq"
+	"github.com/takama/backer/model"
 )
 
 const (
 	// ErrorCodeDuplicateDatabase is code when create database but it already exists
 	ErrorCodeDuplicateDatabase = "42P04"
+	queryGetPlayerBalance      = "SELECT balance FROM players WHERE id = $1"
+	queryInsertPlayer          = "INSERT INTO players (id) VALUES ($1)"
+	queryUpdatePlayer          = "UPDATE players SET balance = $1 WHERE id = $2"
 )
 
 // PostgreSQL implements PostgreSQL driver
@@ -57,6 +61,78 @@ func (pg PostgreSQL) Ready() bool {
 		return true
 	}
 	return false
+}
+
+// Reset makes the DB initialization
+func (pg PostgreSQL) Reset() error {
+	var err error
+	tx, err := pg.Transaction()
+	if err != nil {
+		return err
+	}
+	if err = pg.MigrateDown(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if err = pg.MigrateUp(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+// NewPlayer creates a player with specified ID
+func (pg PostgreSQL) NewPlayer(ID string, tx *sql.Tx) error {
+	stmt, err := tx.Prepare(queryInsertPlayer)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(ID)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// FindPlayer finds existing player by specified ID
+func (pg PostgreSQL) FindPlayer(ID string, tx *sql.Tx) (*model.Player, error) {
+	row := tx.QueryRow(queryGetPlayerBalance, ID)
+	player := &model.Player{ID: ID}
+	err := row.Scan(&player.Balance)
+	return player, err
+}
+
+// SavePlayer saves a Player model
+func (pg PostgreSQL) SavePlayer(player *model.Player, tx *sql.Tx) error {
+	stmt, err := tx.Prepare(queryUpdatePlayer)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(player.Balance, player.ID)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// NewTournament creates a new tournament with specified ID
+func (pg PostgreSQL) NewTournament(ID uint64, tx *sql.Tx) error {
+	return nil
+}
+
+// FindTournament finds existing tournament by specified ID
+func (pg PostgreSQL) FindTournament(ID uint64, tx *sql.Tx) (*model.Tournament, error) {
+	return nil, nil
+}
+
+// SaveTournament saves a Tournament model
+func (pg PostgreSQL) SaveTournament(tournament *model.Tournament, tx *sql.Tx) error {
+	return nil
+}
+
+// Transaction returns DB transaction control
+func (pg PostgreSQL) Transaction() (*sql.Tx, error) {
+	return pg.pool.Begin()
 }
 
 // MigrateUp migrates DB schema
