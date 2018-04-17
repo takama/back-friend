c3fea406c144b3a0cc851342a4ee3da8c705ee43
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-18 01:05:53 +0700

    Added player details tests

--- pkg/handlers/player_test.go
+++ pkg/handlers/player_test.go
@@ -27,4 +27,30 @@ func TestPlayerDetails(t *testing.T) {
 	})
 
 	testHandler(t, handler, http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctrl := bit.NewControl(w, r)
+		ctrl.Params().Set(":id", "")
+		h.Base(h.PlayerDetails)(ctrl)
+	})
+
+	testHandler(t, handler, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+
+	stub.NewPlayer("p1", nil)
+	handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctrl := bit.NewControl(w, r)
+		ctrl.Params().Set(":id", "p1")
+		h.Base(h.PlayerDetails)(ctrl)
+	})
+
+	testHandler(t, handler, http.StatusOK, `{"id":"p1","balance":0}`)
+
+	stub.ErrFind = append(stub.ErrFind, ErrTestError)
+	handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctrl := bit.NewControl(w, r)
+		ctrl.Params().Set(":id", "p1")
+		h.Base(h.PlayerDetails)(ctrl)
+	})
+
+	testHandler(t, handler, http.StatusInternalServerError, ErrTestError.Error())
 }
