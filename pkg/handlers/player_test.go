320f37b210038ba095965cb6adb72a19cca469f3
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-18 21:20:13 +0700

    Added test for player Take

--- pkg/handlers/player_test.go
+++ pkg/handlers/player_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/takama/back-friend/pkg/db"
 
 	"github.com/takama/backer/datastore"
+	"github.com/takama/backer/player"
 )
 
 func TestPlayerDetails(t *testing.T) {
@@ -68,4 +69,64 @@ func TestPlayerFund(t *testing.T) {
 		map[string]string{":id": "p1"},
 		h, h.PlayerFund,
 		http.StatusInternalServerError, ErrTestError.Error())
+
+	stub.ErrFind = append(stub.ErrFind, ErrTestError, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerFund,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerFund,
+		http.StatusOK, `{"id":"p1","balance":300}`)
+}
+
+func TestPlayerTake(t *testing.T) {
+	stub := new(datastore.Stub)
+	stub.Reset()
+	conn := &db.Connection{
+		Config:     config.New(),
+		Controller: stub,
+		Store:      stub,
+	}
+	h := New(conn)
+
+	testHandlerWithParams(t,
+		map[string]string{":id": ""},
+		h, h.PlayerTake,
+		http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1"},
+		h, h.PlayerTake,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	stub.NewPlayer("p1", nil)
+	stub.ErrFind = append(stub.ErrFind, ErrTestError)
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1"},
+		h, h.PlayerTake,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	stub.ErrFind = append(stub.ErrFind, ErrTestError, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerTake,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerTake,
+		http.StatusNotAcceptable, player.ErrInsufficientPoints.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerFund,
+		http.StatusOK, `{"id":"p1","balance":300}`)
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "p1", "data": `{"points": 300}`},
+		h, h.PlayerTake,
+		http.StatusOK, `{"id":"p1","balance":0}`)
 }
