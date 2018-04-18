7c51d7d8d88a4a69a093d2612012ff41fa927c91
Igor Dolzhikov <bluesriverz@gmail.com>
2018-04-19 02:49:02 +0700

    Finished tournament tests

--- pkg/handlers/tournament_test.go
+++ pkg/handlers/tournament_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/takama/back-friend/pkg/db"
 	"github.com/takama/backer/datastore"
 	"github.com/takama/backer/model"
+	"github.com/takama/backer/player"
 	"github.com/takama/backer/tournament"
 )
 
@@ -59,6 +60,12 @@ func TestTournamentAnnounce(t *testing.T) {
 		h, h.TournamentAnnounce,
 		http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
 
+	stub.ErrTx = append(stub.ErrTx, ErrTestError)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1"},
+		h, h.TournamentAnnounce,
+		http.StatusInternalServerError, ErrTestError.Error())
+
 	stub.ErrNew = append(stub.ErrNew, ErrTestError)
 	testHandlerWithParams(t,
 		map[string]string{":id": "1"},
@@ -87,8 +94,7 @@ func TestTournamentAnnounce(t *testing.T) {
 		h, h.TournamentAnnounce,
 		http.StatusOK, `{"id":1,"deposit":300,"is_finished":false,"bidders":[]}`)
 
-	tx, _ := stub.Transaction()
-	stub.SaveTournament(&model.Tournament{ID: 1, IsFinished: true}, tx)
+	stub.SaveTournament(&model.Tournament{ID: 1, IsFinished: true}, nil)
 	testHandlerWithParams(t,
 		map[string]string{":id": "1", "data": `{"deposit": 500}`},
 		h, h.TournamentAnnounce,
@@ -126,6 +132,61 @@ func TestTournamentJoin(t *testing.T) {
 		map[string]string{":id": "1"},
 		h, h.TournamentJoin,
 		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	stub.ErrTx = append(stub.ErrTx, ErrTestError, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	stub.NewPlayer("p1", nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	stub.ErrTx = append(stub.ErrTx, ErrTestError, nil, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"deposit": 300}`},
+		h, h.TournamentAnnounce,
+		http.StatusOK, `{"id":1,"deposit":300,"is_finished":false,"bidders":[]}`)
+
+	stub.NewPlayer("b1", nil)
+	stub.NewPlayer("b2", nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusNotAcceptable, player.ErrInsufficientPoints.Error())
+
+	stub.ErrFind = append(stub.ErrFind, datastore.ErrRecordNotFound, nil, nil, nil, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	stub.ErrTx = append(stub.ErrTx, ErrTestError, nil, nil, nil, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	stub.SavePlayer(&model.Player{ID: "p1", Balance: 100}, nil)
+	stub.SavePlayer(&model.Player{ID: "b1", Balance: 100}, nil)
+	stub.SavePlayer(&model.Player{ID: "b2", Balance: 100}, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1", "backers": ["b1", "b2"]}`},
+		h, h.TournamentJoin,
+		http.StatusOK, `{"id":1,"deposit":300,"is_finished":false,"bidders":[{"id":"p1","winner":false,"prize":0,"backers":["b1","b2"]}]}`)
 }
 
 func TestTournamentResult(t *testing.T) {
@@ -159,4 +220,48 @@ func TestTournamentResult(t *testing.T) {
 		map[string]string{":id": "1"},
 		h, h.TournamentResult,
 		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"deposit": 300}`},
+		h, h.TournamentAnnounce,
+		http.StatusOK, `{"id":1,"deposit":300,"is_finished":false,"bidders":[]}`)
+
+	stub.SavePlayer(&model.Player{ID: "p1", Balance: 300}, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"player": "p1"}`},
+		h, h.TournamentJoin,
+		http.StatusOK, `{"id":1,"deposit":300,"is_finished":false,"bidders":[{"id":"p1","winner":false,"prize":0,"backers":null}]}`)
+
+	stub.ErrFind = append(stub.ErrFind, ErrTestError, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	stub.ErrFind = append(stub.ErrFind, datastore.ErrRecordNotFound, nil, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusNotFound, datastore.ErrRecordNotFound.Error())
+
+	stub.ErrTx = append(stub.ErrTx, ErrTestError, nil, nil)
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusInternalServerError, ErrTestError.Error())
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusOK, `{"id":1,"deposit":300,"is_finished":true,"bidders":[{"id":"p1","winner":true,"prize":300,"backers":null}]}`)
+
+	testHandlerWithParams(t,
+		map[string]string{":id": "1", "data": `{"winners": [{"player": "p1", "prize": 300}]}`},
+		h, h.TournamentResult,
+		http.StatusNotAcceptable, tournament.ErrAllreadyFinished.Error())
 }
